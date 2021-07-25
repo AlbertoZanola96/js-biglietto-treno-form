a94468d065218c62f535d9029cb665d7d4b4e3f6
alberto <albertozanola1996@gmail.com>
2021-07-25 15:38:10 +0200

    Inserimento elementi inziali

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@
 // - Categoria selezionata dall'utente
 // - Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
 // BONUS: predisporre l'interfaccia grafica responsiva
+
+var btnGenera = document.getElementById("prezzoBtn");
+
+btnGenera.addEventListener("click",
+    function() {
+        var nome = document.getElementById("nome").value;
+        var km = document.getElementById("km").value;
+        var fasciaEta = document.getElementById("fascia-eta").value;
+        var biglietto = document.getElementById("biglietto");
+        biglietto.style.display = "block";
+    }
+)
+
+var btnAnnulla = document.getElementById("annulla");
+btnAnnulla.addEventListener("click",
+    function() {
+        var biglietto = document.getElementById("biglietto");
+        biglietto.style.display = "none";
+        document.getElementById("nome").value = "";
+        document.getElementById("km").value = "";
+        document.getElementById("fascia-eta").value = "";
+    }
+)
